516967ede482b5fdbc99c41fdd8bcfc5aec222cb
Sagar Chopra <Chopra.sagar89@gmail.com>
2021-01-17 18:46:39 +0530

    first commit

---
+++ src/components/Events.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button v-on:click="OnButtonClick('Test')">Click Here</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Events',
+
+    methods:{
+        OnButtonClick(message)
+        {
+            console.warn(message + " Button Clicked");
+        }
+    }
+}
+</script>
